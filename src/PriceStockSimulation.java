6924404b3984237070d9f93fc39c19c5b53e6a22
Yuri Khechoyan <KhechoyanY@fontbonne.edu>
2016-09-23 16:38:45 -0500

    Added src files & Required Documentation

---
+++ src/PriceStockSimulation.java
@@ -0,0 +1,25 @@
+/* ========================================================================== */
+/*	PROGRAM Price Stock Machine
+    AUTHOR: Yuri Khechoyan & Jessie Wilkins
+    COURSE NUMBER: CIS 210
+    COURSE SECTION NUMBER: 01
+    INSTRUCTOR NAME: Dr. Tian
+    PROJECT NUMBER: 6
+    DUE DATE: 9/29/2016
+SUMMARY
+	
+	
+INPUT
+
+
+OUTPUT
+
+
+ASSUMPTIONS
+- None
+*/
+public class PriceStockSimulation {
+	public static void main(String[] args){
+		
+	}
+}
